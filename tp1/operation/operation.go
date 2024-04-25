b94b8ee4bc2e410ad9d4141d7ac658c2f9854b1f
Nicolas Severini <nicoseve823@gmail.com>
2024-04-25 11:59:55 -0300

    tp1_en_proceso

--- tp1/operation/operation.go
+++ tp1/operation/operation.go
@@ -19,86 +19,156 @@ const (
 	TERNARY        = "?"
 )
 
-func operate(operator string, stack pila.Pila[int64], stackQuantity *int) (int64, error) {
-	if operator == SQRT {
-		if *stackQuantity < 1 {
-			return 0, fmt.Errorf("stack quantity cannot be less than 1")
-		}
-		operand1 := stack.Desapilar()
-		*stackQuantity--
+type Operation interface {
+	Operate(operands []int64) (int64, error)
+}
 
-		if operand1 < 0 {
-			return 0, fmt.Errorf("operand out of range")
-		}
-		return int64(math.Sqrt(float64(operand1))), nil
-	} else if operator == TERNARY {
-		if *stackQuantity < 3 {
-			return 0, fmt.Errorf("stack quantity cannot be less than 3")
-		}
-		operand3 := stack.Desapilar()
-		*stackQuantity--
-		operand2 := stack.Desapilar()
-		*stackQuantity--
-		operand1 := stack.Desapilar()
-		*stackQuantity--
+type Addition struct{}
 
-		if operand1 != 0 {
-			return operand2, nil
-		} else {
-			return operand3, nil
-		}
-	} else {
-		if *stackQuantity < 2 {
-			return 0, fmt.Errorf("stack quantity cannot be less than 2")
-		}
-
-		operand2 := stack.Desapilar()
-		*stackQuantity--
-		operand1 := stack.Desapilar()
-		*stackQuantity--
-
-		switch operator {
-		case ADDITION:
-			return operand1 + operand2, nil
-		case SUBTRACTION:
-			return operand1 - operand2, nil
-		case MULTIPLICATION:
-			return operand1 * operand2, nil
-		case DIVISION:
-			if operand2 == 0 {
-				return 0, fmt.Errorf("division by zero")
-			}
-			return operand1 / operand2, nil
-		case RAISE:
-			if operand2 < 0 {
-				return 0, fmt.Errorf("power with negative exponent")
-			}
-			return int64(math.Pow(float64(operand1), float64(operand2))), nil
-		case LOGARITHM:
-			if operand2 < 2 {
-				return 0, fmt.Errorf("base of log less than 2")
-			}
-			return int64(math.Log(float64(operand1)) / math.Log(float64(operand2))), nil
-		}
+func (Addition) Operate(operands []int64) (int64, error) {
+	if len(operands) < 2 {
+		return 0, fmt.Errorf("insufficient operands for addition")
 	}
-	return 0, fmt.Errorf("there is no operator")
+	return operands[0] + operands[1], nil
+}
+
+type Subtraction struct{}
+
+func (Subtraction) Operate(operands []int64) (int64, error) {
+	if len(operands) < 2 {
+		return 0, fmt.Errorf("insufficient operands for subtraction")
+	}
+	return operands[0] - operands[1], nil
+}
+
+type Multiplication struct{}
+
+func (Multiplication) Operate(operands []int64) (int64, error) {
+	if len(operands) < 2 {
+		return 0, fmt.Errorf("insufficient operands for multiplication")
+	}
+	return operands[0] * operands[1], nil
+}
+
+type Division struct{}
+
+func (Division) Operate(operands []int64) (int64, error) {
+	if len(operands) < 2 {
+		return 0, fmt.Errorf("insufficient operands for division")
+	}
+	if operands[1] == 0 {
+		return 0, fmt.Errorf("division by zero")
+	}
+	return operands[0] / operands[1], nil
+}
+
+type Power struct{}
+
+func (Power) Operate(operands []int64) (int64, error) {
+	if len(operands) < 2 {
+		return 0, fmt.Errorf("insufficient operands for power operation")
+	}
+	if operands[1] < 0 {
+		return 0, fmt.Errorf("power with negative exponent")
+	}
+	return int64(math.Pow(float64(operands[0]), float64(operands[1]))), nil
+}
+
+type Logarithm struct{}
+
+func (Logarithm) Operate(operands []int64) (int64, error) {
+	if len(operands) < 2 {
+		return 0, fmt.Errorf("insufficient operands for logarithm operation")
+	}
+	if operands[1] < 2 {
+		return 0, fmt.Errorf("base of logarithm less than 2")
+	}
+	return int64(math.Log(float64(operands[0])) / math.Log(float64(operands[1]))), nil
+}
+
+type SquareRoot struct{}
+
+func (SquareRoot) Operate(operands []int64) (int64, error) {
+	if len(operands) < 1 {
+		return 0, fmt.Errorf("insufficient operands for square root operation")
+	}
+	if operands[0] < 0 {
+		return 0, fmt.Errorf("operand out of range for square root")
+	}
+	return int64(math.Sqrt(float64(operands[0]))), nil
+}
+
+type Ternary struct{}
+
+func (Ternary) Operate(operands []int64) (int64, error) {
+	if len(operands) < 3 {
+		return 0, fmt.Errorf("insufficient operands for ternary operation")
+	}
+	if operands[0] != 0 {
+		return operands[1], nil
+	}
+	return operands[2], nil
 }
 
 func isOperator(instruction string) bool {
-	return instruction == "+" ||
-		instruction == "-" ||
-		instruction == "*" ||
-		instruction == "/" ||
-		instruction == "sqrt" ||
-		instruction == "^" ||
-		instruction == "log" ||
-		instruction == "?"
+	operators := "+-*/sqrt^log?" //TODO hacela global
+	return strings.Contains(operators, instruction)
+}
+
+// TODO: cambiar nombre func
+func operate(instruction string, stack pila.Pila[int64]) (int64, error) {
+	operands := make([]int64, 0)
+	var op Operation
+	if instruction == SQRT {
+		for i := 0; i < 1; i++ {
+			if stack.EstaVacia() {
+				return 0, fmt.Errorf("insufficient operands for operator %s", instruction)
+			}
+			operand := stack.Desapilar()
+			operands = append([]int64{operand}, operands...)
+		}
+
+		op = SquareRoot{}
+	} else if instruction == TERNARY {
+		for i := 0; i < 3; i++ {
+			if stack.EstaVacia() {
+				return 0, fmt.Errorf("insufficient operands for operator %s", instruction)
+			}
+			operand := stack.Desapilar()
+			operands = append([]int64{operand}, operands...)
+		}
+		op = Ternary{}
+	} else {
+		for i := 0; i < 2; i++ {
+			if stack.EstaVacia() {
+				return 0, fmt.Errorf("insufficient operands for operator %s", instruction)
+			}
+			operand := stack.Desapilar()
+			operands = append([]int64{operand}, operands...)
+		}
+		switch instruction {
+		case ADDITION:
+			op = Addition{}
+		case SUBTRACTION:
+			op = Subtraction{}
+		case MULTIPLICATION:
+			op = Multiplication{}
+		case DIVISION:
+			op = Division{}
+		case RAISE:
+			op = Power{}
+		case LOGARITHM:
+			op = Logarithm{}
+		}
+	}
+	result, err := op.Operate(operands)
+
+	return result, err
 }
 
 func CalculateExpression(expression string) (int64, error) {
 	instructions := strings.Fields(expression)
 	stack := pila.CrearPilaDinamica[int64]()
-	var stackQuantity int
 
 	for _, instruction := range instructions {
 		if isOperator(instruction) {
@@ -106,17 +176,12 @@ func CalculateExpression(expression string) (int64, error) {
 				return 0, fmt.Errorf("the stack is empty")
 			}
 
-			var result int64
-			var err error
-
-			result, err = operate(instruction, stack, &stackQuantity)
+			result, err := operate(instruction, stack)
 			if err != nil {
-				return 0, fmt.Errorf("problems when operating")
+				return 0, err
 			}
 
 			stack.Apilar(result)
-			stackQuantity++
-
 		} else {
 			number, err := strconv.ParseInt(instruction, 10, 64)
 			if err != nil {
@@ -124,13 +189,13 @@ func CalculateExpression(expression string) (int64, error) {
 			}
 
 			stack.Apilar(number)
-			stackQuantity++
 		}
 	}
+	result := stack.Desapilar()
 
-	if stackQuantity != 1 {
+	if !stack.EstaVacia() {
 		return 0, fmt.Errorf("the content of my stack should have only the result")
 	}
 
-	return stack.Desapilar(), nil
+	return result, nil
 }
